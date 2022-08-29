d2128c06251400bbe8a020a08b060dc622fe861c
OlgaGaletskaya <106168070+OlgaGaletskaya@users.noreply.github.com>
2022-08-29 22:47:39 +0300

    '/'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,7 +26,7 @@ const Card = (props) => {
                     <path d="M-8.19564e-08 5.94345L32 5.94345V3.94345L8.19564e-08 3.94345L-8.19564e-08 5.94345ZM26.9802 9.53917L31.9686 5.95234L30.801 4.32853L25.8126 7.91535L26.9802 9.53917ZM25.7845 1.85047L30.7616 5.53344L31.9512 3.92576L26.9742 0.242787L25.7845 1.85047Z" fill="black"/>
                 </svg>
             </div>
-            <div className='card__price'>{props.item.price} руб.</div>
+            <div className='card__price'>{props.item.price} руб</div>
             <div className='card__amount'>
                 <div className='card__button' onClick={increment}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
